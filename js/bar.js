44bed89ba1b12066f1a6f42b70e4e81d6684e0ee
FJTorrecillas <Ftorrecillas@impulso.edu.uy>
2025-06-15 23:18:54 -0300

    Menú colapsante para la sección de categorías y perfil de usuario en la barra del index

--- js/bar.js
+++ js/bar.js
@@ -1,4 +1,5 @@
 const header = document.querySelector('header');
+const container = document.querySelector('#container');
 let name = document.createElement('div');
 name.id = 'name';
 
@@ -36,9 +37,10 @@ for (let clave in items) {
     let li = document.createElement('li');
     if (items[clave].nombre === active.textContent) {
 	li.innerHTML = `<a class="active" href="${items[clave].enlace}">${items[clave].nombre}</a>`;
-   }
-    else {
-    	li.innerHTML = `<a class="normal" href="${items[clave].enlace}">${items[clave].nombre}</a>`;
+   }else if (items[clave].nombre == 'Categorías &#xF0d7;') {
+    	li.innerHTML = `<a class="normal" id="categories">${items[clave].nombre}</a>`;
+   }else {
+    	li.innerHTML = `<a class="normal" href="${items[clave].enlace}" onclick="alert('Aún no implementado')">${items[clave].nombre}</a>`;
     }
     navega.appendChild(li);
 }
@@ -46,6 +48,62 @@ header.appendChild(name);
 header.appendChild(buscabar);
 header.appendChild(navega);
 header.appendChild(i);
+let state='inactive';
+categories.addEventListener('click', function() {
+	if (state === 'inactive') {
+		let categorymenu = document.createElement('ul');
+		categorymenu.classList.add('listing');
+		categorymenu.classList.add('fromcategory');
+		categorymenu.innerHTML = `
+		<li><a>Vestimenta</a></li>
+		<li><a>Herramientas</a></li>
+		<li><a>Accesorios</a></li>
+		<li><a>Vestimentas</a></li>
+		<li><a>Cocina</a></li>
+		<li><a>Accesorios</a></li>
+		<li><a>Herramientas</a></li>
+		<li><a>Tecnología</a></li>
+		<li><a>Fitness y deporte</a></li>
+		<li><a>Belleza</a></li>
+		<li><a>Entretenimiento</a></li>
+		<li><a>Hogar y muebles</a></li>
+	    `;
+		container.appendChild(categorymenu);
+		state = 'active';
+	}else if (state === 'active') {
+		let categorymenu2 = document.querySelector('.fromcategory');
+          	categorymenu2.remove();
+	    	state = 'inactive';
+	}
+});
+
+let userstatus = "inactive";
+let user = document.querySelector('.fa-circle-user');
+user.addEventListener('click', function() {
+        if (userstatus === "inactive") {
+	    let usermenu = document.createElement('ul');
+	    usermenu.classList.add('listing');
+	    usermenu.classList.add('fromuser');
+	    usermenu.innerHTML = `
+	        <div id="userlogin">
+		     <i class="fa-regular fa-circle-user"></i>
+		     <a href="login.html"><p>Iniciar sesión</p></a>
+		</div>
+		<li><a>Configuración de la cuenta</a></li>
+		<li><a>Permutaciones hechas</a></li>
+		<li><a>Mis publicaciones</a></li>
+		<li><a>Notificaciones</a></li>
+		<li><a>Cerrar sesión</a></li>
+	    `;
+	    container.appendChild(usermenu);
+	    userstatus = "active";
+	}else if (userstatus === "active") {
+	    let usercollapse2 = document.querySelector('.fromuser');
+	    usercollapse2.remove();
+	    userstatus = "inactive";
+	}
+
+});
 
 
 
