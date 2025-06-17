9fd78fcbd1639d9f5299a95694d7f2c6a8759ba7
FJTorrecillas <Ftorrecillas@impulso.edu.uy>
2025-06-17 13:06:39 -0300

    Se agregaron descripciones a las publicaciones existentes

--- js/posts.js
+++ js/posts.js
@@ -1,25 +1,25 @@
 let postsJSON = '{ "posts" : [' +
-'{ "id":0, "imagen":"img/1.jpg", "nombre":"Buzo de lana blanco", "desc":"Usado talla L ancho 61 cm largo 74 cm manga 90 cm", "categoria":"Vestimenta" },' +
+'{ "id":0, "imagen":"img/1.jpg", "nombre":"Buzo de lana blanco", "desc":"Usado talla L, ancho 61cm, largo 74cm, manga 90cm", "categoria":"Vestimenta" },' +
 
-'{ "id":1, "imagen":"img/2.jpg", "nombre":"Katana de acero", "desc":"Descripciones epicas que en algun momento escribí sin un motivo muy exacto", "categoria":"Herramientas" }, '+ 
+'{ "id":1, "imagen":"img/2.jpg", "nombre":"Katana de acero", "desc":"Obra maestra de la forja. Cada detalle de esta katana, desde la empuñadura hasta la hoja, es un testimonio de la habilidad y dedicación del artesano.", "categoria":"Herramientas" }, '+ 
 
-'{ "id":2, "imagen":"img/3.jpg", "nombre":"Anillo de plata 925", "desc":"Descripciones epicas que en algun momento escribí sin un motivo muy exacto", "categoria":"Accesorios" }, ' +
+'{ "id":2, "imagen":"img/3.jpg", "nombre":"Anillo de plata 925", "desc":"El brillo sutil de la aguamarina en un diseño delicado. Este anillo de plata con detalles de bolitas es una pieza única y especial. ¡Ideal para un regalo o para mimarte a ti misma!", "categoria":"Accesorios" }, ' +
 
-'{ "id":3, "imagen":"img/4.jpg", "nombre":"Zapatillas Adidas", "desc":"Descripciones epicas que en algun momento escribí sin un motivo muy exacto", "categoria":"Vestimentas" }, ' +
+'{ "id":3, "imagen":"img/4.jpg", "nombre":"Zapatillas Adidas", "desc":"Zapatillas Adidas, están nuevas, no me quedaban, por eso permuto.", "categoria":"Vestimentas" }, ' +
 
-'{ "id":4, "imagen":"img/5.jpg", "nombre":"Kit de ollas de acero inóxidable", "desc":"Descripciones epicas que en algun momento escribí sin un motivo muy exacto", "categoria":"Herramientas" },' +
+'{ "id":4, "imagen":"img/5.jpg", "nombre":"Kit de ollas de acero inóxidable", "desc":"Ollas muy buenas, casi sin uso. Son 3 ollas de color plateado, vienen con tapa incluída y tienen varios lugares por donde agarrarla por si está muy caliente.", "categoria":"Herramientas" },' +
 
-'{ "id":5, "imagen":"img/6.jpg", "nombre":"Bufanda tejida a mano", "desc":"Descripciones epicas que en algun momento escribí sin un motivo muy exacto", "categoria":"Herramientas" },' +
+'{ "id":5, "imagen":"img/6.jpg", "nombre":"Bufanda tejida a mano", "desc":"Una simple bufanda color más o menos vainilla. Es abrigada, linda y está 100% tejida a mano.", "categoria":"Herramientas" },' +
 
-'{ "id":6, "imagen":"img/7.jpg", "nombre":"Kit de utensilios de cocina", "desc":"Una espatula, un ", "categoria":"Herramientas" },' +
+'{ "id":6, "imagen":"img/7.jpg", "nombre":"Kit de utensilios de cocina", "desc":"Son 11 utensilos de cocina super prácticos con mango de madera.", "categoria":"Herramientas" },' +
 
-'{ "id":7, "imagen":"img/8.jpg", "nombre":"Camisa negra", "desc":"Una camisa negra muy fachera", "categoria":"Herramientas" },' +
+'{ "id":7, "imagen":"img/8.jpg", "nombre":"Camisa negra", "desc":"Una camisa negra muy fachera para ir a fiestas es perfecta, e incluso es lo suficientemente formal como para también utilizarla en reuniones de trabajo e emtrevistas.", "categoria":"Herramientas" },' +
 
-'{ "id":8, "imagen":"img/9.jpg", "nombre":"Renault", "desc":"Descripciones epicas que en algun momento escribí sin un motivo muy exacto", "categoria":"Accesorios" },' +
+'{ "id":8, "imagen":"img/9.jpg", "nombre":"Renault", "desc":"Auto Renault", "categoria":"Accesorios" },' +
 
-'{ "id":9, "imagen":"img/10.jpg", "nombre":"Placa Base", "desc":"Descripciones epicas que en algun momento escribí sin un motivo muy exacto", "categoria":"Tecnología" },' +
+'{ "id":9, "imagen":"img/10.jpg", "nombre":"Placa Base", "desc":"Place base para computadora de escritorio. Es una Intel Desktop Board, tiene lo justo y necesario para el funcionamiento", "categoria":"Tecnología" },' +
 
-'{ "id":10, "imagen":"img/11.jpg", "nombre":"Peluche Makoto Yuki Persona 3 Reload", "desc":"Descripciones epicas que en algun momento escribí sin un motivo muy exacto", "categoria":"Fitness y deporte" }]}';
+'{ "id":10, "imagen":"img/11.jpg", "nombre":"Peluche Makoto Yuki Persona 3 Reload", "desc":"El mejor protagonista (debatible) de la famosa saga de videojuegos japonesa Persona, llegó como peluche para quedarse. Aprovecha la oportunidad de tener este tierno peluche de 15cm para llevarlo a dónde quieras y llorarle en las noches", "categoria":"Fitness y deporte" }]}';
 
 let posts = JSON.parse(postsJSON);
 console.log(posts);
