f58d0e205fa388278b8a7d7110caa049d177ccb5
Nicolás Giménez <nicogimenez73@gmail.com>
2025-08-20 20:23:00 -0300

    ofer, catg y chats hechos, falta su contenido

---
+++ ofertas.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="icon" href="img/logo.png">
+		<link rel="stylesheet" href="css/inicio.css">
+		<link rel="stylesheet" href="css/bar.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+		<link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+		<link href='https://fonts.googleapis.com/css?family=Expletus Sans' rel='stylesheet'>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Ofertas</title>
+	</head>
+	<body>
+		<?php include 'bar.php'; ?>
+		<div id="container">
+			<div id="sectiontitle">
+				<p>Ofertas activas</p>
+			</div>
+			<div id="cards-ofertas"></div>
+		</div>
+		<script src="lib/jquery-3.7.1.min.js.js"></script>
+        
+		<?php
+			header('Content-Type: text/html; charset=UTF-8');
+		?>
+	</body>
+</html>
