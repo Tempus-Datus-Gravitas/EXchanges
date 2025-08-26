263c23e4d8e4026cfa5970338c103cdff1eeaec0
FJTorrecillas <franciscojoeltr@gmail.com>
2025-08-26 11:10:59 -0300

    Cambios muy épicos para lograr registrar usuarios con una base de datos

--- register.php
+++ register.php
@@ -35,47 +35,80 @@
 					<p id="idea"></p>
 				</div>
 			<div id="inputs">
+				<form id="form" method="POST">
 				<div class="part1">
 					<div class="inputspace">
 						<p>Nombre y Apellido</p>
-						<input type="text" id="name" placeholder="">
+						<input type="text" id="name" placeholder="" name="name">
 					</div>
 					<div class="inputspace">
 						<p>Edad</p>
-						<input type="text" id="age" placeholder="">
+						<input type="text" id="age" placeholder="" name="age">
 					</div>
 					<div class="inputspace">
 						<p>Nombre de usuario</p>
-						<input type="text" id="username" placeholder="">
+						<input type="text" id="username" placeholder="" name="username">
 					</div>
 					<p class="nobtn" id="next">Siguiente</p>
-
+					
 				</div>
 				<div class="part2">
-
-					<div class="inputspace">
-						<p>Correo</p>
-						<input type="email" id="email" placeholder="">
-					</div>
-					<div class="inputspace">
-						<p>Contraseña</p>
-						<input type="password" id="password" placeholder="">
-					</div>
-					<div class="inputspace">
-						<p>Repetir contraseña</p>
-						<input type="password" id="password2" placeholder="">
-					</div>
-						<p class="nobtn" id="registrarse">Registrarse</p>
+						<div class="inputspace">
+							<p>Correo</p>
+							<input type="email" id="email" placeholder="" name="email">
+						</div>
+						<div class="inputspace">
+							<p>Contraseña</p>
+							<input type="password" id="password" placeholder="" name="password">
+						</div>
+						<div class="inputspace">
+							<p>Repetir contraseña</p>
+							<input type="password" id="password2" placeholder="" name="password2">
+						</div>
+							<p class="nobtn" id="registrarse" onclick="document.getElementById('submit').click();">Registrarse</p>
+							<input type="submit" id="submit" style="display:none;">
+					</form>
 				</div>
 			</div>
 
-
-
 			</div>
-
-
 		</div>
 		<script src="lib/jquery-3.7.1.min.js.js"></script>
 		<script src="js/signlog.js"></script>
+		<?php
+			if (ISSET($_POST['name']) && ISSET($_POST['age']) && ISSET($_POST['username']) && ISSET($_POST['email']) && ISSET($_POST['password']) && ISSET($_POST['password2'])) {
+				//El codigo evita inyecciones SQL y verifica si el usuario existe en la base de datos, lo segundo es obvio.
+				require_once 'conexion.php';
+				$stmt = $conn->prepare("SELECT * FROM users WHERE email = ? OR username = ?");
+				$email = $_POST['email'];
+				$username = $_POST['username'];
+				$stmt->bind_param("ss", $email, $username); 
+				$stmt->execute(); 
+				$result = $stmt->get_result();
+				if ($result->num_rows > 0) {
+					echo "<script>alert('El correo o nombre de usuario ya están en uso');</script>";
+
+				}else {
+					$stmt = $conn->prepare("INSERT INTO users (username, name, email, password, verified, admin) VALUES (?, ?, ?, ?, 0, 0)");
+					$name = $_POST['name'];
+					$age = $_POST['age'];
+					$password = $_POST['password'];
+					$email = $_POST['email'];
+					$username = $_POST['username'];
+					$stmt->bind_param("ssss", $username, $name, $email, $password);
+					if ($stmt->execute()) {
+						echo "<script>alert('Registro exitoso');</script>";
+						session_start();
+						$_SESSION['user_id'] = $conn->insert_id;
+						$_SESSION['user_username'] = $username;
+						$_SESSION['user_email'] = $email;
+						$_SESSION['user_admin'] = 0;
+						$_SESSION['user_verified'] = 0;
+						session_regenerate_id(true);
+						echo "<script>window.location.href = 'index.php';</script>";	
+					}
+				}
+			}
+		?>
 	</body>
 </html>
