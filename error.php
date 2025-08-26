4838ed3b5b9e74abd9fcabf7f955ffacb7f1bfae
FJTorrecillas <franciscojoeltr@gmail.com>
2025-08-26 11:12:43 -0300

    Como que el nombre de los archivos explica bien

---
+++ error.php
@@ -0,0 +1,28 @@
+<?php
+	include 'conexion.php';
+	if ($conn) {
+		header("Location: index.php");
+		exit();
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="icon" href="img/logo.png">
+		<link rel="stylesheet" href="css/error.css">
+		<link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+		<link href='https://fonts.googleapis.com/css?family=Expletus Sans' rel='stylesheet'>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Error</title>
+	</head>
+	<body> 
+		<div id="container">
+			<div id="content">
+				<h1>Error de conexión</h1>
+				<p>No se pudo conectar a la base de datos. Por favor, inténtalo de nuevo más tarde.</p>
+				<button onclick="location.reload();">Reintentar</button>
+			</div>
+		</div>
+	</body>
+</html>
